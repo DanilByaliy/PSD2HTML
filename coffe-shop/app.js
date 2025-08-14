8707e6b876db1e6f472ca6026df185c8340a8b78
DanilByaliy <byaliy03@gmail.com>
2025-08-14 18:36:59 +0300

    feat(coffee-shop): add slider functionality for welcome section

--- coffe-shop/app.js
+++ coffe-shop/app.js
@@ -1,6 +1,22 @@
-const swiper = new Swiper(".swiper", {
+const welcomeSwiper = new Swiper(".welcome__swiper", {
   slidesPerView: 1,
+  initialSlide: 1,
   speed: 600,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: {
+    invert: false,
+    thresholdDelta: 50,
+    forceToAxis: true,
+  },
+});
+
+const swiper = new Swiper(".cites__swiper", {
+  slidesPerView: 1,
+  speed: 1000,
   centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
