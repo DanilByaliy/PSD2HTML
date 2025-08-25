1cac604de957ce085f292f523c4e7a500e290791
DanilByaliy <byaliy03@gmail.com>
2025-08-25 21:40:33 +0300

    feat(honey): add responsive header with burger menu functionality

--- honey/src/main.js
+++ honey/src/main.js
@@ -1,1 +1,14 @@
 import "./scss/styles.scss";
+
+let burger = document.querySelector(".header__burger");
+let menu = document.querySelector(".header__menu");
+
+function toggleBurger() {
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("header__burger--active");
+    menu.classList.toggle("header__menu--active");
+    document.body.classList.toggle("body--lock");
+  });
+}
+
+toggleBurger();
