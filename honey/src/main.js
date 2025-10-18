9bf30ddc3d5c357abb5309b3d8be303d1f5d62b2
DanilByaliy <byaliy03@gmail.com>
2025-10-18 18:16:26 +0300

    feat(honey): add cite and gallery sections layout and its responsive styles

--- honey/src/main.js
+++ honey/src/main.js
@@ -1,4 +1,10 @@
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "./scss/styles.scss";
+
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 import { useDynamicAdapt } from "./tools/dynamic-adapt.js";
 
 useDynamicAdapt();
@@ -15,3 +21,73 @@ function toggleBurger() {
 }
 
 toggleBurger();
+
+window.addEventListener("DOMContentLoaded", () => {
+  const swiperWrapper = document.querySelector(".gallery__wrapper");
+
+  const resizableSwiper = (
+    breakpoint,
+    swiperClass,
+    swiperSettings,
+    callback,
+  ) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
+      }
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        swiperWrapper.classList.add("swiper-wrapper");
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        swiperWrapper.classList.remove("swiper-wrapper");
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener("change", checker);
+    checker();
+  };
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on("slideChange", function (e) {
+        console.log("*** mySwiper.activeIndex", instance.activeIndex);
+      });
+    }
+  };
+
+  resizableSwiper(
+    "(max-width: 767px)",
+    ".gallery__slider",
+    {
+      loop: true,
+      spaceBetween: 20,
+      // slidesPerView: 3,
+      slidesPerView: "auto",
+      speed: 1000,
+      initialSlide: 1,
+      centeredSlides: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      mousewheel: {
+        invert: false,
+        thresholdDelta: 30,
+        forceToAxis: true,
+      },
+      modules: [Navigation, Pagination],
+    },
+    someFunc,
+  );
+});
